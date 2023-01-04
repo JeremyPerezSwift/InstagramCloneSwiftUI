164ddbfeb155df1b34abd98104731f2d1c9e571a
JeremyPerezSwift <jeremyperez34000@gmail.com>
2023-01-04 12:39:53 +0100

    Initial Commit

---
+++ InstagramCloneSwiftUI/InstagramCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneSwiftUIApp.swift
+//  InstagramCloneSwiftUI
+//
+//  Created by Jérémy Perez on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
